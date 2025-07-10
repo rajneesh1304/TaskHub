a7b6ea95e24c723cdf70ac2a468bca975b3140b2
RAJNEESH <163464676+rajneesh1304@users.noreply.github.com>
2025-07-10 22:19:09 +0530

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -51,12 +51,8 @@ app.use(
 app.get(
   `/`,
   asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-    throw new BadRequestException(
-      "This is a bad request",
-      ErrorCodeEnum.AUTH_INVALID_TOKEN
-    );
-    return res.status(HTTPSTATUS.OK).json({
-      message: "Hello Subscribe to the channel & share",
+    res.status(HTTPSTATUS.OK).json({
+      message: "Hello, I am Rajneesh!
     });
   })
 );
